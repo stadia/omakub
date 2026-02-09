68a7d00dbdfad9f867a5f87140988ff4b33280a7
Luca Pattocchio <pattocchioluca@gmail.com>
2026-02-09 17:58:50 +0100

    Update Spotify GPG key in app-spotify.sh

--- install/desktop/optional/app-spotify.sh
+++ install/desktop/optional/app-spotify.sh
@@ -3,7 +3,7 @@
 # Stream music using https://spotify.com
 if [ ! -f /etc/apt/sources.list.d/spotify.list ]; then
   [ -f /etc/apt/trusted.gpg.d/spotify.gpg ] && sudo rm /etc/apt/trusted.gpg.d/spotify.gpg
-  curl -sS https://download.spotify.com/debian/pubkey_C85668DF69375001.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+  curl -sS https://download.spotify.com/debian/pubkey_5384CE82BA52C83A.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
   echo "deb [signed-by=/etc/apt/trusted.gpg.d/spotify.gpg] https://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 fi
 
